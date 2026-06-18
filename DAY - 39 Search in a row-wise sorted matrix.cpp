826f5fcc9636f02ede8cf94fc5507f14e3ef1feb
Vikranth V <vvikranth08@gmail.com>
2026-06-19 02:20:59 +0530

    Create DAY - 39 Search in a row-wise sorted matrix.cpp

---
+++ DAY - 39 Search in a row-wise sorted matrix.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    bool searchRowMatrix(vector<vector<int>> &mat, int x) {
+        for(auto &row : mat){
+            if(binary_search(row.begin(),row.end(),x)){
+                return true;
+            }
+        }
+        return false;
+    }
+};
